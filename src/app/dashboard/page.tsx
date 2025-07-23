281756738fa086e839f5cd0b4c4db426eeaa0bd9
Joao Victor Carvalho Machado <machado.joaovictor@outlook.com>
2025-07-23 16:12:13 -0300

    feat: Enhance Dashboard page with Textarea and Button components for task input

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,6 +2,8 @@ import { Metadata } from "next";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 import { authOptions } from "../lib/auth";
+import Textarea from "@/components/textarea/Textarea";
+import Button from "@/components/button/Button";
 
 export const metadata: Metadata = {
     title: "Dashboard"
@@ -16,8 +18,35 @@ export default async function DashboardPage() {
     }
 
     return (
-        <main>
-            <h1>Dashboard Page</h1>
+        <main className="w-full">
+            <section className="bg-primary-background text-on-dark flex items-center justify-center">
+                <div className="max-w-5xl w-full px-5 pb-7 mt-14">
+                    <h1 className="mb-4 font-bold text-4xl">Qual é a sua tarefa?</h1>
+                    <form action="" className="text-on-light flex flex-col gap-4">
+
+                        <Textarea
+                            placeholder="Digite a sua tarefa"
+                            className="bg-secondary-background"
+                        />
+
+                        <label htmlFor="customCheckbox" className="flex gap-3 items-center">
+                            <input
+                                type="checkbox"
+                                className="peer hidden"
+                                name=""
+                                id="customCheckbox"
+                            />
+                            <div className="w-8 h-8 border-2 border-secondary-background flex flex-row items-center justify-center rounded-lg peer-checked:after:content-['✔'] peer-checked:after:text-on-dark peer-checked:after:text-xl transition" />
+                            <span className="text-on-dark font-normal">Deixar tarefa pública</span>
+                        </label>
+
+                        <Button className="bg-blue-500 text-on-dark border-blue-500">
+                            Registrar
+                        </Button>
+
+                    </form>
+                </div>
+            </section>
         </main>
     );
 }
--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,55 +1,37 @@
+import clsx from "clsx";
 import Link from "next/link";
+import { ButtonHTMLAttributes } from "react";
 
-interface ButtonProps {
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
     children: React.ReactNode;
     onClick?: () => void;
     href?: string;
-    variant?: 'primary' | 'secondary';
-    size?: 'sm' | 'md' | 'lg';
-    rounded?: 'none' | 'full';
     className?: string;
 }
 
 export default function Button(
     {
         children,
-        onClick,
         href,
-        variant = 'primary',
-        size = 'md',
-        rounded = 'none',
         className,
-    }
-        : ButtonProps
+        ...rest
+    }: ButtonProps
 ) {
-
-    const baseClasses = 'bg-transparent border-2 cursor-pointer transition duration-150';
-
-    const sizeClasses = {
-        sm: 'px-4 py-2 text-lg',
-        md: 'px-10 py-3.5 text-lg',
-        lg: 'px-16 py-4 text-xl',
-    };
-
-    const variantClasses = {
-        primary: 'bg-primary-background text-on-dark border-on-dark hover:bg-secondary-background hover:text-on-light hover:border-on-light',
-        secondary: 'bg-secondary-background text-on-light border-on-light border-on-light hover:bg-primary-background hover:text-on-dark hover:border-on-dark',
-    };
-
-    const roundedClasses = {
-        none: 'rounded-none',
-        full: 'rounded-full',
-    }
-
-    const allClasses = `${baseClasses} ${sizeClasses[size]} ${variantClasses[variant]} ${roundedClasses[rounded]} ${className || ''}`;
+    const baseStyle = 'py-3.5 border-2 rounded-lg cursor-pointer transition duration-150 font-bold hover:opacity-90 active:opacity-85';
 
     return (
         href ?
-            <Link href={href} className={allClasses}>
+            <Link
+                href={href}
+                className={clsx(baseStyle, className)}
+            >
                 {children}
             </Link>
             :
-            <button onClick={onClick} className={allClasses}>
+            <button
+                className={clsx(baseStyle, className)}
+                {...rest}
+            >
                 {children}
             </button>
     );
