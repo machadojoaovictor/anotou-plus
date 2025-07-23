f6fab83d6ca244900e09a892c2336f17bac98e2f
Joao Victor Carvalho Machado <machado.joaovictor@outlook.com>
2025-07-23 16:11:59 -0300

    feat: Add Textarea component with clsx for dynamic styling

---
+++ src/components/textarea/Textarea.tsx
@@ -0,0 +1,17 @@
+import clsx from "clsx";
+import { HTMLProps } from "react";
+
+export default function Textarea(
+    {
+        className, ...rest
+    }: HTMLProps<HTMLTextAreaElement>
+) {
+    const baseStyle = 'w-full resize-none h-40 rounded-lg outline-none py-4 px-4 text-lg';
+
+    return (
+        <textarea
+            className={clsx(baseStyle, className)}
+            {...rest}
+        />
+    );
+}
