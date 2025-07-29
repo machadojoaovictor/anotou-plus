b7deb8fc5e6a01ca8d15517392b516bdcfb4adad
Joao Victor Carvalho Machado <machado.joaovictor@outlook.com>
2025-07-29 04:08:50 -0300

    refactor: Streamline DashboardPage by removing unused imports and integrating DashboardForm

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,52 +1,27 @@
-import { Metadata } from "next";
-import { getServerSession } from "next-auth";
-import { redirect } from "next/navigation";
-import { authOptions } from "../lib/auth";
-import Textarea from "@/components/textarea/Textarea";
-import Button from "@/components/button/Button";
-
-export const metadata: Metadata = {
-    title: "Dashboard"
-}
+import DashboardForm from "./DashboardForm";
+import { auth } from "@/auth";
+import TaskList from "@/components/task-list/TaskList";
 
 export default async function DashboardPage() {
 
-    const session = await getServerSession(authOptions);
-
-    if (!session?.user) {
-        redirect("/");
-    }
+    const session = await auth();
+    const user = session?.user;
 
     return (
-        <main className="w-full">
+        <main className="w-full flex flex-col">
             <section className="bg-primary-background text-on-dark flex items-center justify-center">
                 <div className="max-w-5xl w-full px-5 pb-7 mt-14">
                     <h1 className="mb-4 font-bold text-4xl">Qual é a sua tarefa?</h1>
-                    <form action="" className="text-on-light flex flex-col gap-4">
-
-                        <Textarea
-                            placeholder="Digite a sua tarefa"
-                            className="bg-secondary-background"
-                        />
-
-                        <label htmlFor="customCheckbox" className="flex gap-3 items-center">
-                            <input
-                                type="checkbox"
-                                className="peer hidden"
-                                name=""
-                                id="customCheckbox"
-                            />
-                            <div className="w-8 h-8 border-2 border-secondary-background flex flex-row items-center justify-center rounded-lg peer-checked:after:content-['✔'] peer-checked:after:text-on-dark peer-checked:after:text-xl transition" />
-                            <span className="text-on-dark font-normal">Deixar tarefa pública</span>
-                        </label>
-
-                        <Button className="bg-blue-500 text-on-dark border-blue-500">
-                            Registrar
-                        </Button>
-
-                    </form>
+                    <DashboardForm userId={user?.email} />
+                </div>
+            </section>
+            <section className="">
+                <div className="mt-16 flex flex-col gap-8 ">
+                    <h1 className="font-bold text-4xl text-center">Minhas tarefas</h1>
+                    <TaskList userEmail={user?.email as string} />
                 </div>
             </section>
         </main>
     );
 }
+
