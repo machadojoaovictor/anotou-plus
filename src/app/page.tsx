0049446080e7ad63b581a87db8c4152fb576af63
Joao Victor Carvalho Machado <machado.joaovictor@outlook.com>
2025-08-12 11:40:38 -0300

    feat: implement dynamic post and comment count on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,23 @@
 import Image from "next/image";
 
 import heroImg from '../../public/images/hero.png';
+import { db } from "@/services/firebaseConnection";
+import { collection, getDocs } from "firebase/firestore";
 
-export default function HomePage() {
+export default async function HomePage() {
+
+  const taskRef = collection(db, "tasks");
+  const taskSnapshot = await getDocs(taskRef);
+  const posts = taskSnapshot.size;
+
+  let comments = 0;
+
+  for (const taskDoc of taskSnapshot.docs) {
+    const commentRef = collection(db, "tasks", taskDoc.id, "comments");
+    const commentSnapshot = await getDocs(commentRef);
+    comments += commentSnapshot.size;
+  }
+
   return (
     <main className="bg-primary-background w-full h-[calc(100vh-6rem)] flex flex-col gap-7 justify-center items-center">
       <div>
@@ -20,8 +35,8 @@ export default function HomePage() {
           seus estudos e tarefas
         </h1>
         <div className="flex items-center justify-center gap-4">
-          <span className="bg-secondary-background text-on-light px-16 py-3.5 font-bold rounded-2xl">+ 7 mil posts</span>
-          <span className="bg-secondary-background text-on-light px-16 py-3.5 font-bold rounded-2xl">+ 1 mil comentários</span>
+          <span className="bg-secondary-background text-on-light px-16 py-3.5 font-bold rounded-2xl">{posts} posts</span>
+          <span className="bg-secondary-background text-on-light px-16 py-3.5 font-bold rounded-2xl">{comments} comentários</span>
         </div>
       </div>
     </main>
